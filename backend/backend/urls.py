c4d7cfd23967916f762825433e746e2fdbcf02b3
yourusernameglaubervilanegit config --global user.email glauberfmarques@gmail.comgit config --global user.name yourusernameglaubervilane <glauberfmarques@gmail.com>
2023-12-13 06:57:51 -0700

    Implement the API and serialization to launch endpoints from REACT future implementation

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,8 +15,26 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+# import views from todo
+from todo import views
+
+# import routers from the REST framework
+# it is necessary for routing
+from rest_framework import routers
+
+# create a router object
+router = routers.DefaultRouter()
+
+# register the router
+router.register(r'tasks',views.TodoView, 'task')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # add another path to the url patterns
+    # when you visit the localhost:8000/api
+    # you should be routed to the django Rest framework
+    path('api/', include(router.urls))
 ]
